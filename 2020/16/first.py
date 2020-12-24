9c642b1fc78e8df3a8db517316a6750d796dde06
Antoine Gagniere <antoine.gagniere@yahoo.com>
2020-12-24 17:03:45 +0100

    Update first.py

--- 2020/16/first.py
+++ 2020/16/first.py
@@ -6,23 +6,16 @@ class Constraint:
         return True in [item in r for r in self.ranges]
 
 
-def parse_ticket(string: str):
-    return [int(field) for field in string.split(',')]
-
-
 if __name__ == '__main__':
     with open('input.txt') as my_input_file:
         lines = my_input_file.read().splitlines()
-        constraints = {}
+        constraints = []
         for line in lines[:20]:
             key, value = line.split(': ')
-            constraints[key] = Constraint([interval.split('-') for interval in value.split(' or ')])
-
-        mine = parse_ticket(lines[22])
-
+            constraints += [Constraint([interval.split('-') for interval in value.split(' or ')])]
         result = 0
         for line in lines[25:]:
             for v in map(int, line.split(',')):
-                if True not in [v in constraint for constraint in constraints.values()]:
+                if True not in [v in constraint for constraint in constraints]:
                     result += v
         print("Errors sum :", result)
