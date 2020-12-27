3b52e64e34547e2b7dfd0096419209daea9e9a30
Antoine Gagniere <antoine.gagniere@yahoo.com>
2020-12-27 17:03:24 +0100

    Update second.py

--- 2020/17/second.py
+++ 2020/17/second.py
@@ -14,16 +14,10 @@ class Bounds:
 X, Y, Z, W = range(4)
 world = set()
 bounds = Bounds([8, 8, 1, 1])
-# world.add((1,0,0,0))
-# world.add((2,1,0,0))
-# world.add((0,2,0,0))
-# world.add((1,2,0,0))
-# world.add((2,2,0,0))
 for y in bounds[Y]:
     for x, c in enumerate(input()):
         if c == '#':
             world.add((x, y, 0, 0))
-
 for _ in range(6):
     prev = world
     world = set()
@@ -37,11 +31,12 @@ for _ in range(6):
                                                       y + ((i // 3) % 3) - 1,
                                                       z + ((i // 9) % 3) - 1,
                                                       w + (i // 27) - 1) in prev])
+                    #neighbours = len([i for i in range(3 ** 4) if i != 3**4 // 2 and tuple([c + ((i // 3 ** j) % 3) - 1 for j, c in enumerate([x, y, z, w])]) in prev])
                     if neighbours == 3 or (neighbours == 2 and (x, y, z, w) in prev):
                         world.add((x, y, z, w))
-    for z in bounds[Z]:
-        print('-' * (5 + (bounds[X].stop - bounds[X].start + 1) * (bounds[W].stop - bounds[W].start)))
-        for y in bounds[Y]:
-            print("{:2}{:3}".format(z if not y else ' ', y), ' '.join([''.join(['.#'[(x, y, z, w) in world] for x in bounds[X]]) for w in bounds[W]]))
+    # for z in bounds[Z]:
+    #     print('-' * (5 + (bounds[X].stop - bounds[X].start + 1) * (bounds[W].stop - bounds[W].start)))
+    #     for y in bounds[Y]:
+    #         print("{:2}{:3}".format(z if not y else ' ', y), ' '.join([''.join(['.#'[(x, y, z, w) in world] for x in bounds[X]]) for w in bounds[W]]))
 
 print(len(world))
