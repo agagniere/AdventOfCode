30a52d39931d862950673a4674517170f4c47b11
Antoine Gagniere <antoine.gagniere@yahoo.com>
2020-12-27 16:34:44 +0100

    Update second.py

--- 2020/17/second.py
+++ 2020/17/second.py
@@ -1,33 +1,25 @@
 class Bounds:
-    def __init__(self, size):
-        self.min_x = self.min_y = self.min_z = self.min_w = 0
-        self.max_x = self.max_y = size
-        self.max_z = self.max_w = 1
+    def __init__(self, upper_limits):
+        self.limits = [[0, end] for end in upper_limits]
 
-    def x(self): return range(self.min_x, self.max_x)
-    def y(self): return range(self.min_y, self.max_y)
-    def z(self): return range(self.min_z, self.max_z)
-    def w(self): return range(self.min_w, self.max_w)
+    def __getitem__(self, item):
+        return range(*self.limits[item])
 
     def grow(self):
-        self.min_x -= 1
-        self.min_y -= 1
-        self.min_z -= 1
-        self.min_w -= 1
-        self.max_x += 1
-        self.max_y += 1
-        self.max_z += 1
-        self.max_w += 1
+        for dim in self.limits:
+            dim[0] -= 1
+            dim[1] += 1
 
 
+X, Y, Z, W = range(4)
 world = set()
-bounds = Bounds(8)
+bounds = Bounds([8, 8, 1, 1])
 # world.add((1,0,0,0))
 # world.add((2,1,0,0))
 # world.add((0,2,0,0))
 # world.add((1,2,0,0))
 # world.add((2,2,0,0))
-for y in bounds.y():
+for y in bounds[Y]:
     for x, c in enumerate(input()):
         if c == '#':
             world.add((x, y, 0, 0))
@@ -36,21 +28,20 @@ for _ in range(6):
     prev = world
     world = set()
     bounds.grow()
-    for w in bounds.w():
-        for z in bounds.z():
-            for y in bounds.y():
-                for x in bounds.x():
+    for w in bounds[W]:
+        for z in bounds[Z]:
+            for y in bounds[Y]:
+                for x in bounds[X]:
                     neighbours = len([i for i in range(3 ** 4)
-                                      if i != ((3**4)//2) and (x + (i % 3) - 1,
+                                      if i != 3**4 // 2 and (x + (i % 3) - 1,
                                                       y + ((i // 3) % 3) - 1,
                                                       z + ((i // 9) % 3) - 1,
                                                       w + (i // 27) - 1) in prev])
                     if neighbours == 3 or (neighbours == 2 and (x, y, z, w) in prev):
                         world.add((x, y, z, w))
-    # for z in bounds.z():
-    #     print('')
-    #     for y in bounds.y():
-    #         print("{:2}{:3}".format(z, y), end=' ')
-    #         print(' '.join([''.join(['.#'[(x, y, z, w) in world] for x in bounds.x()]) for w in bounds.w()]))
+    for z in bounds[Z]:
+        print('-' * (5 + (bounds[X].stop - bounds[X].start + 1) * (bounds[W].stop - bounds[W].start)))
+        for y in bounds[Y]:
+            print("{:2}{:3}".format(z if not y else ' ', y), ' '.join([''.join(['.#'[(x, y, z, w) in world] for x in bounds[X]]) for w in bounds[W]]))
 
 print(len(world))
