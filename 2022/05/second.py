9feb0659dced8af9b852491f57926026282a5396
Antoine <angagnie@student.42.fr>
2022-12-10 08:12:18 +0100

    Refactor day 5 2022

--- 2022/05/second.py
+++ 2022/05/second.py
@@ -1,26 +1,12 @@
 from utils import *
 
-drawing = []
-for line in lines():
-    drawing += [line]
-    if not '[' in line:
-        break
-print('\n'.join(drawing))
-
-stacks = [list() for _ in drawing[0][::4]]
-for col, stack in filter(lambda p:p[1].isdigit(), enumerate(drawing[-1])):
-    stacks[int(stack) - 1] = [row[col] for row in reversed(drawing[:-1]) if row[col].isalpha()]
+drawing = next(non_empty_line_group(lines()))
+stacks = extract_stacks(drawing)
 
 for line in lines():
-    if not line:
-        continue
-    *count, src, dst = filter(str.isdigit, line)
-    count, src, dst = map(int, [''.join(count), src, dst])
+    count, src, dst = map(int, filter(str.isdigit, line.split()))
     move = stacks[src - 1][-count:]
     del stacks[src - 1][-count:]
     stacks[dst - 1] += move
-    #print('------')
-    #for s in stacks:
-    #    print('[' + '] ['.join(s) + ']')
 
 print(''.join([stack[-1] for stack in stacks]))
--- 2022/05/utils.py
+++ 2022/05/utils.py
@@ -1,8 +1,23 @@
-import sys
-
 def lines():
     while True:
         try:
             yield input()
         except:
             break
+
+def non_empty_line_group(iterable):
+    acc = []
+    for line in iterable:
+        if line:
+            acc.append(line)
+        elif acc:
+            yield acc
+            acc = []
+    if acc:
+        yield acc
+
+def extract_stacks(drawing):
+    stacks = [list() for _ in drawing[0][::4]]
+    for col, stack in filter(lambda p:p[1].isdigit(), enumerate(drawing[-1])):
+        stacks[int(stack) - 1] = [row[col] for row in reversed(drawing[:-1]) if row[col].isalpha()]
+    return stacks
