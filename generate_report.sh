9153967680870c56be63112bc1e5ed46a3360bf2
Antoine <antoine.gagniere@orolia2s.com>
2023-12-10 20:19:54 +0100

    Move readme generator script to a dedicated file
    Display total of stars per year and a Grand Total

---
+++ generate_report.sh
@@ -0,0 +1,42 @@
+set -e # Exit on fail
+set -u # Exit if using an undefined variable
+set -o pipefail # A pipe fails if one of its command fails
+shopt -s extglob lastpipe # Persist hashmap values outside of loops
+
+declare -A stars # Declare a hashmap
+
+printf "| Day " ;
+for folder in $("ls" -d */)
+do
+    printf "|[${folder%/}](https://adventofcode.com/${folder%/})"
+done
+echo "|"
+
+printf "| --: " ;
+for folder in $("ls" -d */)
+do
+    printf "| :--------: "
+    stars[$folder]=0
+done
+echo "|"
+
+for i in $(seq -w 25)
+do
+    printf "| $i  "
+    for folder in $("ls" -d */)
+    do
+        printf "|"
+        compgen -G "$folder$i/first*" >/dev/null && printf ":star": && stars[$folder]=$(( ${stars[$folder]} + 1 ))
+        compgen -G "$folder$i/second*" > /dev/null && printf ":star:" && stars[$folder]=$(( ${stars[$folder]} + 1 ))
+    done
+    printf "\n"
+done
+
+counter=0
+printf "| Total "
+for folder in $("ls" -d */)
+do
+    printf "| %i " ${stars[$folder]}
+	counter=$(( counter + ${stars[$folder]} ))
+done
+printf "\n\nTotal stars: %i" $counter
