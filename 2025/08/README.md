db8814d9987c35ea7037ba67ed4987bf3b761221
Antoine GAGNIERE <antoine.gagniere@datadoghq.com>
2025-12-09 01:07:42 +0100

    2025 08 part 2

---
+++ 2025/08/README.md
@@ -0,0 +1,21 @@
+# Day 8
+
+[Statement](https://adventofcode.com/2025/day/8)
+
+## Timings
+
+### Sample
+```
+info: [08] parse: 6.292us, ~314ns/line
+info: [08] prepare: 5.333us, ~28ns/link
+info: [08] part1: 6.417us
+info: [08] part2: 3.708us
+```
+
+### Input
+```
+info: [08] parse: 40.584us, ~40ns/line
+info: [08] prepare: 5.302ms, ~10ns/link
+info: [08] part1: 294.291us
+info: [08] part2: 536.042us
+```
--- 2025/08/solve.zig
+++ 2025/08/solve.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 const parse = @import("parse.zig").parse;
-const build_edges = @import("graph.zig").build_edges;
 const part1 = @import("part1.zig").part1;
+const part2 = @import("part2.zig").part2;
+const graph = @import("graph.zig");
 
 const Allocator = std.mem.Allocator;
 const Reader = std.Io.Reader;
@@ -17,15 +18,23 @@ fn solve(alloc: Allocator, input: *Reader) !struct { u64, u64 } {
     defer alloc.free(jboxes);
     const tp = timer.lap();
 
-    var links = try build_edges(alloc, jboxes);
+    const jboxes2 = try alloc.dupe(graph.Node, jboxes);
+    defer alloc.free(jboxes2);
+    var links = try graph.build_edges(alloc, jboxes);
     defer links.deinit();
+    var links2 = try graph.build_edges(alloc, jboxes2);
+    defer links2.deinit();
     const te = timer.lap();
 
     const p1 = try part1(alloc, &links);
     const t1 = timer.lap();
 
+    const p2 = try part2(alloc, &links2, jboxes.len);
+    const t2 = timer.lap();
+
     std.log.info("[08] parse: {D}, ~{D}/line", .{ tp, tp / jboxes.len });
     std.log.info("[08] prepare: {D}, ~{D}/link", .{ te, te / links.capacity() });
     std.log.info("[08] part1: {D}", .{t1});
-    return .{ p1, 0 };
+    std.log.info("[08] part2: {D}", .{t2});
+    return .{ p1, p2 };
 }
